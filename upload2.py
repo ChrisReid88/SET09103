3ebd9caf2e8006c2cdeaa787c83e4999f03d7aac
ChrisReid88 <40202859@live.napier.ac.uk>
2017-09-28 11:51:44 +0000

    Chapter 4 of workbook

---
+++ upload2.py
@@ -0,0 +1,29 @@
+from flask import Flask, url_for, request
+app = Flask(__name__)
+
+@app.route("/display/")
+def display():
+  return '<img src="'+url_for('static',filename='upload/file.png')+'"/>'
+
+@app.route("/upload/",methods=['POST','GET'])
+def account():
+  if request.method == 'POST':
+    f = request.files['datafile']
+    f.save('static/uploads/file.png')
+    return "File Uploadsed"
+  else:
+    page='''
+    <html>
+    <body>
+    <form action="" method="post" name="form" enctype="multipart/form-data">
+      <input type="file" name="datafile" />
+      <input type="submit" name="submit" id="submit"/>
+    </form>
+    </body>
+    </html>
+    '''
+
+    return page, 200
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
